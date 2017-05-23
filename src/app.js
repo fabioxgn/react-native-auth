54b259c8708f30630f79840a1b72139d46f9c0ed
Fábio Gomes <fabioxgn@gmail.com>
2017-05-23 20:56:26 -0300

    Adiciona componentes do projeto album
    Adiciona configuração do firebase
    
    Adiciona input para email
    
    Adicona input para senha
    
    Adiciona lógica de login
    
    Implementa logout

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDUvDm8xuF3hMM8zBvSpq95Xl4UbwXD6og',
+      authDomain: 'react-native-auth-96b7c.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-96b7c.firebaseio.com',
+      projectId: 'react-native-auth-96b7c',
+      storageBucket: 'react-native-auth-96b7c.appspot.com',
+      messagingSenderId: '835423298544'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return this.renderLogout();
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  renderLogout() {
+    return (
+      <Card>
+        <CardSection>
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText='Authentication' />
+        {this.renderContent()}
       </View>
     );
   }
